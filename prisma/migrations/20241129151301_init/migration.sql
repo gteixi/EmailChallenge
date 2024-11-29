cd0edef864c7235a1fcbae860316f1a9cb4a41e7
gteixi <gemmateixido25@gmail.com>
2024-11-29 17:21:35 +0100

    chore: update schema and apply migration

--- prisma/migrations/20241129151301_init/migration.sql
+++ prisma/migrations/20241129151301_init/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "Message" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "from" TEXT NOT NULL,
     "subject" TEXT NOT NULL,
     "body" TEXT NOT NULL,
     "tags" TEXT NOT NULL,
